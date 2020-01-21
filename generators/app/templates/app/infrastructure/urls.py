8c1f44c5d1208dae3b7af3a0726cf8751f80b2e2
Mark Kenneth Ranosa <mark@pearlpay.io>
2020-01-21 16:15:54 +0800

    [EW-186] :construction: :truck: Moved identical scaffold artifacts to app subgenerator

--- generators/app/templates/app/infrastructure/urls.py
+++ generators/app/templates/app/infrastructure/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from infrastructure.views import <%= namePascalCase %>ViewSet
-from infrastructure.views.<%= nameSnakeCase %> import <%= namePascalCase %>ViewsAPI
-
 
 class OptionalSlashRouter(SimpleRouter):
 
@@ -13,10 +10,8 @@ class OptionalSlashRouter(SimpleRouter):
 
 
 router = OptionalSlashRouter()
-router.register('<%= nameDashSeparated %>', <%= namePascalCase %>ViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('<%= nameDashSeparated %>/<int:<%= nameSnakeCase %>_id>/increment-view', <%= namePascalCase %>ViewsAPI.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
