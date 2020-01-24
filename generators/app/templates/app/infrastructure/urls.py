908456402bddfbfc550f03858dfee6605472b78f
Mark Kenneth Ranosa <mark@pearlpay.io>
2020-01-24 14:33:51 +0800

    :construction: Removed unused code for urls setup

--- generators/app/templates/app/infrastructure/urls.py
+++ generators/app/templates/app/infrastructure/urls.py
@@ -2,16 +2,16 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 
-class OptionalSlashRouter(SimpleRouter):
+# class OptionalSlashRouter(SimpleRouter):
 
-    def __init__(self):
-        self.trailing_slash = '/?'
-        super(SimpleRouter, self).__init__()
+#     def __init__(self):
+#         self.trailing_slash = '/?'
+#         super(SimpleRouter, self).__init__()
 
 
-router = OptionalSlashRouter()
+# router = OptionalSlashRouter()
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
